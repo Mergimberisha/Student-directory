945caed675123851e9fb9a8688978df492beadbc
Mergim <mergimberisha0@gmail.com>
2023-04-05 14:27:19 +0100

    Added cohart to the users input

--- directory_exercise.rb
+++ directory_exercise.rb
@@ -1,15 +1,15 @@
 def input_students
-  puts "Please enter the name, age, country and country of birth of the student. Seperated by a comma ','".center(50)
+  puts "Please enter the name, cohort, age, country and country of birth of the student. Seperated by a comma ','".center(50)
   puts "To finish, just hit return twice".center(50)
   # create an empty array
   students = []
   # getting more information from the user
   information = gets.chomp.split(",")
-  name, age, country, birth = information
+  name, cohort, age, country, birth = information
   # while the name is not empty, repeat this code
   while !name.empty?
     # add the student hash to the array
-    students << { name: name, age: age, country: country, birth: birth, cohort: :november }
+    students << { name: name, age: age, country: country, birth: birth, cohort: cohort }
     puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
@@ -27,7 +27,7 @@ end
 def print(students)
   count = 0
   while count < students.length
-    puts "#{count + 1}. #{students[count][:name]} is #{students[count][:age]} years old from#{students[count][:country]} and is born in #{students[count][:birth]} (#{students[count][:cohort]} cohart)".center(50)
+    puts "#{count + 1}. #{students[count][:name]} is#{students[count][:age]} years old from#{students[count][:country]} and is born in#{students[count][:birth]} and is a#{students[count][:cohort]} cohort".center(50)
     count += 1
   end
 end
