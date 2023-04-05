9ad2fca75f1a08a039d45a665a9db4f8f04766fc
Mergim <mergimberisha0@gmail.com>
2023-04-05 13:19:19 +0100

    Adding .center() method to align my ouput

--- directory_exercise.rb
+++ directory_exercise.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the name, age, country and country of birth of the student. Seperated by a comma ','"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name, age, country and country of birth of the student. Seperated by a comma ','".center(50)
+  puts "To finish, just hit return twice".center(50)
   # create an empty array
   students = []
   # getting more information from the user
@@ -10,7 +10,7 @@ def input_students
   while !name.empty?
     # add the student hash to the array
     students << { name: name, age: age, country: country, birth: birth, cohort: :november }
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
   end
@@ -19,21 +19,21 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 # This while loop now takes more information from the users input
 def print(students)
   count = 0
   while count < students.length
-    puts "#{count + 1}. #{students[count][:name]} is #{students[count][:age]} years old from#{students[count][:country]} and is born in #{students[count][:birth]} (#{students[count][:cohort]} cohart)"
+    puts "#{count + 1}. #{students[count][:name]} is #{students[count][:age]} years old from#{students[count][:country]} and is born in #{students[count][:birth]} (#{students[count][:cohort]} cohart)".center(50)
     count += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
