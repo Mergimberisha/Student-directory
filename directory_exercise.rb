5807b3646026a3aa4a93133ad8a2dba499e9515f
Mergim <mergimberisha0@gmail.com>
2023-04-06 13:57:13 +0100

    Adding an if statment to print an empty student error

--- directory_exercise.rb
+++ directory_exercise.rb
@@ -7,12 +7,12 @@ def input_students
   information = gets.strip.split(",")
   name, cohort, age, country, birth = information
   # while the name is not empty, repeat this code
-  while !name.empty?
+  while !information.empty?
     # add the student hash to the array
     students << { name: name, age: age, country: country, birth: birth, cohort: cohort }
     puts "Now we have #{students.count} students".center(50)
     # get another name from the user
-    name = gets.strip
+    information = gets.strip
   end
   # return the array of students
   students
@@ -26,9 +26,19 @@ end
 # This while loop now takes more information from the users input
 def print(students)
   count = 0
-  while count < students.length
-    puts "#{count + 1}. #{students[count][:name]} is#{students[count][:age]} years old from#{students[count][:country]} and is born in#{students[count][:birth]} and is a#{students[count][:cohort]} cohort".center(50)
-    count += 1
+  if students.length == 0
+    puts "There are no students in your list".center(50)
+  else
+    while count < students.length
+      if students[count][:cohort].nil?
+        puts "#{count + 1}. #{students[count][:name]} is#{students[count][:age]} years old from#{students[count][:country]} and is born in#{students[count][:birth]} and is a#{students[count][:cohort]} cohort".center(50)
+        puts "WARNING! This student has missing information".center(50)
+        count += 1
+      else
+        puts "#{count + 1}. #{students[count][:name]} is#{students[count][:age]} years old from#{students[count][:country]} and is born in#{students[count][:birth]} and is a#{students[count][:cohort]} cohort".center(50)
+        count += 1
+      end
+    end
   end
 end
 
